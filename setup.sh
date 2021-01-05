0352aba729f195b441ea59e7153c06fd5a38d1b0
Mr.Green <tnusraddinov@gmail.com>
2021-01-05 19:03:26 +0300

    gpg-agent update

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt -y upgrade
 
 #generic tools
-sudo apt install -y git curl jq tmux htop ubuntu-drivers-common software-properties-common 
+sudo apt install -y git curl jq tmux htop gpg-agent ubuntu-drivers-common software-properties-common 
 
 #setup python
 sudo apt install -y python3 python3-pip
@@ -22,7 +22,7 @@ sudo pip3 install gunicorn #gunicorn should be installed with sudo
 sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
 
 # Docker + Compose (logout/login needed)
-sudo apt install -y gpg-agent apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
