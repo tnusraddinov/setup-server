cae2f5351bcf04261cf67ff5b2e3424a49d30373
Mr.Green <tnusraddinov@gmail.com>
2021-01-08 17:30:58 +0300

    postgresql install fix

--- setup.sh
+++ setup.sh
@@ -35,7 +35,7 @@ docker-compose --version
 # PostgreSQL 
 sudo apt-get install -y wget ca-certificates
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb [arch=amd64] http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
 sudo apt-get update
 sudo apt-get install -y postgresql-12 postgresql-contrib
 
